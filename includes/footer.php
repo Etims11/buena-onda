83b3aed523804cd07e72ede24db73327975fced2
Yannick FORMONT <formont.yannick@icloud.com>
2016-02-16 14:34:51 +0100

    Responsive du contenu de la Home

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <footer>
 	<p>
-		© Buena Onda Restauration Franco-Argentine - <a href="mentions.php">Mentions légales</a><br />
-		Buena Onda Wagram - 55, rue de Prony, 75017 Paris - 01 77 15 82 25<br />
-		Buena Onda Batignolles - 75, rue Legendre, 75017 Paris - 01 45 89 20 90<br />
+		© Buena Onda Restauration Franco-Argentine - <a href="mentions.php">Mentions&nbsp;légales</a><br />
+		Buena Onda Wagram - 55, rue de Prony, 75017 Paris - 0177158225<br />
+		Buena Onda Batignolles - 75, rue Legendre, 75017 Paris - 0145892090<br />
 		<a class="author" href="http://manonzampieri.com/ZM/fr/" target="_blank">Design by Manon ZAMPIERI</a> - 
 		<a class="author" href="http://formontyannick.com/" target="_blank">Developed by Yannick FORMONT</a>
 	</p>
